4f2a893dd21eea88d0d5e3b73b127d8be7136b30
Willyan Rodrigues <154492788+willyanr@users.noreply.github.com>
2025-08-18 20:48:32 -0300

    Delete backend/grapes_api directory

--- backend/grapes_api/grapes_api/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grapes_api.settings')
-
-app = Celery('grapes_api')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
--- backend/grapes_api/landingpages/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class LandingpagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'landingpages'
-
